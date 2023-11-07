049c8f2ae2c363de55a1d9ad2e02dbec6a5a86a1
Luca Noviello <novielloluca12@gmail.com>
2023-11-07 22:14:51 +0100

    created basic updating tickrate system. cookies now update every 50ms added Clicker which currently only adds 0.2 cookies if u have 15, UI does not function correctly. single mouse click on cookie adds 1 cookie.

--- Cookie Clicker/MainWindow.xaml.cs
+++ Cookie Clicker/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Cookie_Clicker
 {
@@ -20,9 +21,65 @@ namespace Cookie_Clicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double cookies = 0;
+        private double cookiesPerSecond = 0;
+        
+
+        DispatcherTimer cookieTimer = new DispatcherTimer();
+        DispatcherTimer gameTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            cookieTimer.Interval = TimeSpan.FromSeconds(1);
+            cookieTimer.Tick += CookieTimer_Tick;
+            cookieTimer.Start();
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(50);
+            gameTimer.Tick += gameTimer_tick;
+            gameTimer.Start();
+
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            cookies++;
+            LblCookie.Content = cookies + " Cookies";
+        }
+        private void gameTimer_tick(object sender, EventArgs e)
+        {
+            LblCookie.Content = cookies + " Cookies";
+            LblCookiePerSecond.Content = cookiesPerSecond + "s";
+            ClickerVerify();
+
+
+        }
+        private void CookieTimer_Tick(object sender, EventArgs e)
+        {
+            cookies = cookies + cookiesPerSecond;
+            
+
+            
+
+        }
+        private void ClickerVerify()
+        {
+            if (cookies < 15)
+            {
+                ClickerP.IsEnabled = false;
+            }
+            else
+            {
+                ClickerP.IsEnabled = true;
+                ClickerP.Background = new SolidColorBrush(Colors.AliceBlue);
+
+            }
+        }
+        private void ClickerP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ClickerVerify();
+            cookiesPerSecond = cookiesPerSecond + 0.2;
+            cookies = cookies - 15;
         }
     }
 }
